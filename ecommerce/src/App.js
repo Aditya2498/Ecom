1bf5236b382b88277f82bbe75beaf17815caffa7
Adi-Elite <adityaelitemantra@gmail.com>
2021-11-06 19:24:03 +0530

    bootstrap theme added

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,8 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 
 const App = () =>{
   return (
     <>
+    <Header/>
+    <main>
+      <Container>
     <h1>Welcome to Ecom!</h1>
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
